d92e306bdfdb299da8a84df4418cd1f254408f12
eoeo2255 <naiga6@gmail.com>
2023-04-25 11:01:48 +0900

    중복 제거

--- src/main/java/org/example/N42862/Main.java
+++ src/main/java/org/example/N42862/Main.java
@@ -1,28 +1,54 @@
 package org.example.N42862;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 }
 
 class Solution {
 
-    private int borrow;
+    private int borrowCount;
+    private Set<Integer> borrowClothes = new HashSet<>();
+    private Set<Integer> extraClothes = new HashSet<>();
 
     public int solution(int n, int[] lost, int[] reserve) {
-        canIReserve(lost, reserve);
-        return (n - lost.length) + borrow;
+        borrowSuccess(lost, reserve);
+        return (n - lost.length) + borrowCount;
     }
 
-    public int canIReserve(int[] lost, int[] reserve) {
-        borrow = 0;
+    public int borrowSuccess(int[] lost, int[] reserve) {
 
         for (int i = 0; i < lost.length; i++) {
             for (int j = 0; j < reserve.length; j++) {
-
-                if (lost[i] > reserve[j]-2 && lost[i] < reserve[j]+2)
-                    borrow++;
+                if (canIReserve(lost[i], reserve[j])) {
+                    duplication(lost[i], reserve[j]);
                 }
             }
-        return borrow;
+        }
+        return borrowCount;
+    }
+
+    public boolean canIReserve(int lost, int reserve) {
+        if (lost > reserve-2 && lost < reserve+2) {
+            return true;
+        }
+        return false;
+    }
+
+    public int duplication(int lost, int reserve) {
+        borrowClothes.add(lost);
+        extraClothes.add(reserve);
+
+        if (borrowClothes.size() == extraClothes.size()) {
+            borrowCount = extraClothes.size();
+        } else if (borrowClothes.size() < extraClothes.size()) {
+            borrowCount = borrowClothes.size();
+        } else if (borrowClothes.size() > extraClothes.size()) {
+            borrowCount = extraClothes.size();
+        }
+
+        return borrowCount;
     }
 
 }
--- src/test/java/org/example/N42862/Tests.java
+++ src/test/java/org/example/N42862/Tests.java
@@ -1,10 +1,13 @@
 package org.example.N42862;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@TestMethodOrder(MethodOrderer.MethodName.class)
 public class Tests {
 
     @Test
@@ -26,4 +29,86 @@ public class Tests {
                 4
         );
     }
+
+    @Test
+    @DisplayName("n-> 7 / lost-> [2, 3, 5] / reserve-> [1] = 5")
+    void t02_2() {
+        assertThat(
+                new Solution().solution(7,new int[]{2,3,5},new int[]{1})
+        ).isEqualTo(
+                5
+        );
+    }
+
+    @Test
+    @DisplayName("n-> 5 / lost-> [1, 2, 3, 4, 5] / reserve-> [1, 2, 3, 4, 5] = 5")
+    void t03() {
+        assertThat(
+                new Solution().solution(5,new int[]{1, 2, 3, 4, 5},new int[]{1, 2, 3, 4, 5})
+        ).isEqualTo(
+                5
+        );
+    }
+
+    @Test
+    @DisplayName("n-> 5 / lost-> [1, 2, 3, 4, 5] / reserve-> [1] = 1")
+    void t04() {
+        assertThat(
+                new Solution().solution(5,new int[]{1, 2, 3, 4, 5},new int[]{1})
+        ).isEqualTo(
+                1
+        );
+    }
+
+    @Test
+    @DisplayName("n-> 5 / lost-> [1, 2, 3] / reserve-> [5] = 2")
+    void t05() {
+        assertThat(
+                new Solution().solution(5,new int[]{1, 2, 3},new int[]{5})
+        ).isEqualTo(
+                2
+        );
+    }
+
+    @Test
+    @DisplayName("n-> 5 / lost-> [1, 2] / reserve-> [3, 4, 5] = 4")
+    void t06() {
+        assertThat(
+                new Solution().solution(5,new int[]{1, 2},new int[]{3, 4, 5})
+        ).isEqualTo(
+                4
+        );
+    }
+
+    @Test
+    @DisplayName("n-> 5 / lost-> [1] / reserve-> [3, 4, 5] = 4")
+    void t07() {
+        assertThat(
+                new Solution().solution(5,new int[]{1},new int[]{3, 4, 5})
+        ).isEqualTo(
+                4
+        );
+    }
+
+    @Test
+    @DisplayName("n-> 5 / lost-> [1] / reserve-> [1, 2, 3, 4, 5] = 5")
+    void t08() {
+        assertThat(
+                new Solution().solution(5,new int[]{1},new int[]{1, 2, 3, 4, 5})
+        ).isEqualTo(
+                5
+        );
+    }
+
+    @Test
+    @DisplayName("n-> 5 / lost-> [1, 3, 17] / reserve-> [3] = 3")
+    void t09() {
+        assertThat(
+                new Solution().solution(5,new int[]{1, 3, 17},new int[]{3})
+        ).isEqualTo(
+                3
+        );
+    }
+
+
 }
