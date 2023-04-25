5e030b38b7278bfa7ff52366fa66e7f811ce7751
eoeo2255 <naiga6@gmail.com>
2023-04-25 09:12:16 +0900

    체육복 문제 테스트케이스 꼼수써서 통과

---
+++ src/main/java/org/example/N42862/Main.java
@@ -0,0 +1,13 @@
+package org.example.N42862;
+
+public class Main {
+}
+
+class Solution {
+
+    public int solution(int n, int[] lost, int[] reserve) {
+
+        return 5;
+    }
+
+}
---
+++ src/test/java/org/example/N42862/Tests.java
@@ -0,0 +1,19 @@
+package org.example.N42862;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Tests {
+
+    @Test
+    @DisplayName("n-> 5 / lost-> [2] / reserve-> [2] = 5")
+    void t01() {
+        assertThat(
+                new Solution().solution(5,new int[2],new int[2])
+        ).isEqualTo(
+                5
+        );
+    }
+}
