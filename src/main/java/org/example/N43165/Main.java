00c151a7a34c364d66ce70cb5a8b6fe1e7dd8e39
eoeo2255 <naiga6@gmail.com>
2023-05-11 20:59:28 +0900

    재귀함수

--- src/main/java/org/example/N43165/Main.java
+++ src/main/java/org/example/N43165/Main.java
@@ -1,16 +1,41 @@
 package org.example.N43165;
 
 public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(new int[]{1, 1, 1}, 3));
+    }
 }
 
 
 class Solution{
 
     public int solution(int[] numbers, int target) {
-        int answer = 0;
-        return answer;
+        return new NumberOfCases(numbers, target).calc();
+    }
+}
+
+class NumberOfCases {
+    private final int[] numbers;
+    private final int target;
+
+    public NumberOfCases(int[] numbers, int target) {
+        this.numbers = numbers;
+        this.target = target;
     }
 
+    int calc() {
+        return calc(0, 0);
+    }
+
+    private int calc(int depth, int sum) {
+        //  마지막까지 갔을 때, sum 값이 target 과 같으면 1 반환
+        if (depth == numbers.length) return sum == target ? 1 : 0;
+
+        // 그렇지 않으면 계속 탐색
+        return calc(depth + 1, sum + numbers[depth])        //  + 값
+                + calc(depth + 1, sum - numbers[depth]);        //  - 값
+    }
 
 
 }
