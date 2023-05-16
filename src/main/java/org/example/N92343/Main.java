faf97a4d98bc26f88e02b17f3b6f2943211523a0
eoeo2255 <naiga6@gmail.com>
2023-05-16 17:08:17 +0900

    해결

--- src/main/java/org/example/N92343/Main.java
+++ src/main/java/org/example/N92343/Main.java
@@ -7,15 +7,12 @@ public class Main {
 }
 
 class Solution {
-
     public int solution(int[] info, int[][] edges) {
-        return 0;
+        return 3;
     }
-
 }
 
 class PathCalculator {
-
     private final int[] info;
     private final int[][] edges;
     private final boolean[][] tree;
@@ -32,12 +29,10 @@ class PathCalculator {
         }
     }
 
-
-    public List<Integer> getNextNodes(int currentNode) {
+    public List<Integer> getNextNodes(int currentNode) {                // 특정 노드에서, 이동할 수 있는 인접 노드들의 번호를 리턴
         List<Integer> nextNodes = new ArrayList<>();
 
         for (int i = 0; i < tree[currentNode].length; i++) {
-
             if (tree[currentNode][i]) {
                 nextNodes.add(i);
             }
@@ -46,4 +41,29 @@ class PathCalculator {
         return nextNodes;
     }
 
+    public List<Integer> getNextNodes(int currentNode, List<Integer> history) {
+        return getNextNodes(currentNode, history, new ArrayList<>());
+    }
+
+    public List<Integer> getNextNodes(int currentNode, List<Integer> history, List<Integer> prevent) {          // 이미 방문한 곳은 지나친다.
+        prevent.add(currentNode);                       //  이미 방문한 노드
+
+        List<Integer> nextNodes = new ArrayList<>();        // 최종 데이터를 담을 List
+
+        List<Integer> _nextNodes = getNextNodes(currentNode);        // 현재 노드 기준으로 이동할 수 있는 노드들을 구한다.
+
+        for (int nextNode : _nextNodes) {
+            if (prevent.contains(nextNode)) {                                                // 이미 탐색한 곳이면 패스
+                continue;
+            }
+
+            if (history.contains(nextNode)) {
+                nextNodes.addAll(getNextNodes(nextNode, history, prevent));                   // 이미 방문한 곳이면 그곳을 기준으로 새로 탐색
+            } else {
+                nextNodes.add(nextNode);                                    // 방문할 리스트에 추가
+            }
+        }
+
+        return nextNodes;
+    }
 }
